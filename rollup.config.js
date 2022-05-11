9d51d986e506a4350016e62d4ac105c14bada75e
findnr <504875043@qq.com>
2022-05-11 09:34:18 +0800

    2022-05-11 09:34:17

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 程英明
  * @Date: 2022-05-10 09:54:28
- * @LastEditTime: 2022-05-10 09:58:52
+ * @LastEditTime: 2022-05-11 08:20:59
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \use-findjs\rollup.config.js
@@ -9,6 +9,8 @@
  */
 import typescript from "rollup-plugin-typescript";
 import sourceMaps from "rollup-plugin-sourcemaps";
+import { terser } from "rollup-plugin-terser";
+import cleanup from "rollup-plugin-cleanup"
 export default {
     input: "./src/index.ts",
     plugins: [
@@ -16,20 +18,24 @@ export default {
             exclude: "node_modules/**",
             typescript: require("typescript")
         }),
-        sourceMaps()
+        sourceMaps(),
+        terser(),
+        cleanup(),
+
     ],
     output: [
+        // {
+        //     format: "cjs",
+        //     file: "./lib/bundle.cjs.js",
+        //     sourcemap: true
+        // },
+        // {
+        //     format: "es",
+        //     file: "./lib/bundle.esm.js",
+        //     sourcemap: true
+        // }, 
         {
-            format: "cjs",
-            file: "lib/bundle.cjs.js",
-            sourcemap: true
-        },
-        {
-            format: "es",
-            file: "lib/bundle.esm.js",
-            sourcemap: true
-        }, {
-            file: "lib/ctools.mini.js",
+            file: "./lib/ctools.mini.js",
             format: "umd",
             name: "ctools",
         }
--- src/local/index.ts
+++ src/local/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: 程英明
  * @Date: 2022-05-08 15:25:36
- * @LastEditTime: 2022-05-09 22:36:45
+ * @LastEditTime: 2022-05-11 08:18:15
  * @LastEditors: 程英明
  * @Description: 
  * @FilePath: \use-findjs\src\local\index.ts
